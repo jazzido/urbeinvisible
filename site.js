4de327ec71f753a646170da46c0b3cdf4d01d79c
Manuel Aristarán <jazzido@jazzido.com>
2013-12-26 01:38:25 -0300

    cosas

--- site.js
+++ site.js
@@ -1,4 +1,5 @@
 $(function() {
+
   var resetAll = function() {
     $('div.city').animate({ opacity: 1 })
     $("body").css("overflow", "auto");
@@ -15,27 +16,40 @@ $(function() {
                             function() {
                               $('div.city').not(city).animate({ opacity: 0 })
                               sentence.animate({ backgroundColor: '#FFFFC0' });
-                              $("body").css("overflow", "hidden"); // disable scrolling
-                              window.location.hash = sentence.attr('href');
+                              $("body")
+                              .css("overflow", "hidden") // disable scrolling
+                              .addClass('focused');
+
+                              window.location.hash = sentence.attr('href').substring(0, sentence.attr('href').length - 1);
                             });
   };
 
   $('.city a[href^="#"]').on('click',function (e) {
+    if ($('body').hasClass('focused')) return;
+
     e.preventDefault();
 
-    var target = this.hash,
-	$target = $(target);
+    var target = this.hash;
+
+    console.log(target);
 
     $('.city a').css('background-color', '');
-
     var sentence = $('a' + target);
     var city = sentence.closest('div.city');
 
     smoothScroll(sentence, city);
   });
 
+  $('body').click(function(e) {
+    if ($(this).hasClass('focused')) {
+      e.preventDefault(); e.stopPropagation();
+      resetAll();
+      $(this).removeClass('focused');
+    }
+  });
+
   if (window.location.hash !== '') {
-    var sentence = $('a' + window.location.hash);
+    var sentence = $('a' + window.location.hash + '_');
     var city = sentence.closest('div.city');
 
     smoothScroll(sentence, city);
