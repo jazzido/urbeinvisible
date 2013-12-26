600eb4732e0647c892d1c668ce48b0a6c4a52170
Manuel Aristarán <jazzido@jazzido.com>
2013-12-26 17:08:17 -0300

    yeah

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "http://rubygems.org"
 gem 'nokogiri'
+gem 'twitter', '~>5.0.0.rc.1'
 gem 'chatterbot'
---
+++ urbeinvisible.rb
@@ -0,0 +1,46 @@
+require 'twitter'
+require 'json'
+
+twitter_conf = File.open('./twitter_credentials.json') { |f|
+  JSON.parse(f.read)
+}
+secciones = File.open('./data.json') { |f|
+  JSON.parse(f.read)
+}
+already_sent = File.open('./already_sent.json') { |f|
+  JSON.parse(f.read)
+} rescue []
+
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = twitter_conf['consumer_key']
+  config.consumer_secret     = twitter_conf['consumer_secret']
+  config.access_token        = twitter_conf['access_token']
+  config.access_token_secret = twitter_conf['access_token_secret']
+end
+
+
+short_url_length = client.configuration[:short_url_length]
+
+sentences = secciones.map { |k, v| v['sentences'] }.flatten
+
+# encontrar oraciones candidatas
+# largo menor a 140 - largo_url - 1
+c = sentences.select { |s|
+  already_sent.find { |sent| sent['sentence'] == s }.nil? \
+  && s.length.between?(5, 140 - (short_url_length + 1))
+}
+
+candidate_sentence = c.sample
+candidate_sentence_idx = sentences.index(candidate_sentence)
+
+status_msg = "#{candidate_sentence} http://urbeinvisible.nerdpower.org/#s_#{candidate_sentence_idx}"
+
+client.update(status_msg)
+
+already_sent << { 'date' => Time.now.to_f, 'sentence' => candidate_sentence }
+
+File.open('./already_sent.json', 'w') { |f|
+  JSON.dump(already_sent,
+            f)
+}
